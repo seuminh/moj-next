bd4318c805d298f93b1d17d7bca1dc8ccd409c30
chumsrun <chumsrun@gmail.com>
2021-03-30 11:49:54 +0700

    add breadcrumb

--- components/Breadcrumb.js
+++ components/Breadcrumb.js
@@ -1,9 +1,44 @@
-const Breadcrumb = () => {
-   return (
-      <div className="pageInfo">
-         <h1>ព័ត៌មានមន្រ្តីរាជការ</h1>
-      </div>
-   );
+import { useRouter } from "next/router";
+import { Breadcrumb } from "antd";
+import { useState, useEffect } from "react";
+import Link from "next/link";
+
+const BreadcrumbComponent = () => {
+  const router = useRouter();
+  const [breadcrumbList, setBreadcrumbList] = useState([]);
+  useEffect(() => {
+    let vid = router.asPath;
+    let srun = [];
+    let cheat = vid.split("/");
+    if (vid == "/") {
+      srun = [{ link: "/", name: "Home" }];
+    } else {
+      cheat.forEach((v, i) => {
+        i == 0
+          ? srun.push({ link: "/", name: "Home" })
+          : srun.push({
+              link: srun[srun.length - 1].link + v + "/",
+              name: cheat[i][0].toUpperCase() + cheat[i].slice(1),
+            });
+      });
+    }
+    setBreadcrumbList(srun);
+  }, [router]);
+  console.log(router);
+  return (
+    <div className="pageInfo">
+      {/* <h1>ព័ត៌មានមន្រ្តីរាជការ</h1> */}
+      <Breadcrumb>
+        {breadcrumbList.map((v) => {
+          return (
+            <Breadcrumb.Item>
+              <Link href={v.link}>{v.name}</Link>
+            </Breadcrumb.Item>
+          );
+        })}
+      </Breadcrumb>
+    </div>
+  );
 };
 
-export default Breadcrumb;
+export default BreadcrumbComponent;
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 const Index = () => {
    return (
       <div>
+         
          <h1>HomePage</h1>
       </div>
    );
