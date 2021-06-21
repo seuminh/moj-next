9046750fda6d1b74ee0e09900d958460608abed3
chumsrun <chumsrun@gmail.com>
2021-06-21 12:19:58 +0700

    increase fontsize on search icon

--- components/Header.js
+++ components/Header.js
@@ -5,99 +5,94 @@ import { Button, Input, AutoComplete } from "antd";
 import { useRouter } from "next/router";
 import { useState } from "react";
 import highlightJSX from "@/utils/highlightJSX";
-import debounceFn from "@/utils/debounceFn";
 
 const Header = () => {
-   const [session, loading] = useSession();
-   const router = useRouter();
-   const [valueSearch, setValueSearch] = useState("");
-   const [options, setOptions] = useState([]);
-   const handleSearch = async (value) => {
-      setValueSearch(value);
-      debounceFn(searchUsers)(value)
-   };
-   const searchUsers = async(value) => {
-      console.log(value);
-      const { data: users } = await fetch(
-         "/api/users?searchTerm=" + value.toLowerCase()
-      ).then((res) => res.json());
-      setOptions(
-         value
-            ? users.map((v) => {
-                 const reg = new RegExp(value, "gi");
-                 return {
-                    value: v.id,
-                    label: (
-                       <span>
-                          <strong>firstName</strong>:
-                          {highlightJSX(reg, v.firstName)} |{" "}
-                          <strong>lastName</strong>:{" "}
-                          {highlightJSX(reg, v.lastName)} |{" "}
-                          <strong>nationalityIDNum</strong>:{" "}
-                          {highlightJSX(reg, v.nationalityIDNum)}
-                       </span>
-                    ),
-                 };
-              })
-            : []
-      );
-   }
+  const [session, loading] = useSession();
+  const router = useRouter();
+  const [valueSearch, setValueSearch] = useState("");
+  const [options, setOptions] = useState([]);
+  const handleSearch = async (value) => {
+    setValueSearch(value);
+    const { data: users } = await fetch(
+      "/api/users?searchTerm=" + value.toLowerCase()
+    ).then((res) => res.json());
+    setOptions(
+      value
+        ? users.map((v) => {
+            const reg = new RegExp(value, "gi");
+            return {
+              value: v.id,
+              label: (
+                <span>
+                  <strong>firstName</strong>:{highlightJSX(reg, v.firstName)} |{" "}
+                  <strong>lastName</strong>: {highlightJSX(reg, v.lastName)} |{" "}
+                  <strong>nationalityIDNum</strong>:{" "}
+                  {highlightJSX(reg, v.nationalityIDNum)}
+                </span>
+              ),
+            };
+          })
+        : []
+    );
+  };
 
-   const onSelect = (value) => {
-      setValueSearch(valueSearch);
-      router.push("/employee/" + value);
-   };
+  const onSelect = (value) => {
+    setValueSearch(valueSearch);
+    router.push("/employee/" + value);
+  };
 
-   return (
-      <div className="header">
-         <AutoComplete
-            dropdownMatchSelectWidth={252}
-            options={options}
-            className="search"
-            style={{ minWidth: "700px" }}
-            onSelect={onSelect}
-            value={valueSearch}
-            onSearch={handleSearch}
-         >
-            <Input placeholder="ស្វែងរក" suffix={<SearchOutlined />} />
-         </AutoComplete>
-         <div className="headerInfo">
-            <div>
-               <p>
-                  ក្រសួង-ស្ថាប័ន
-                  <span style={{ color: "#6a0e00", fontWeight: "bold" }}>
-                     យុត្តិធម័
-                  </span>
-               </p>
-            </div>
-            <div>
-               <p>
-                  ឈ្មោះអ្នកចូលប្រើ{" "}
-                  <span style={{ color: "#6a0e00", fontWeight: "bold" }}>
-                     {session && session.user?.firstName}
-                  </span>
-               </p>
-            </div>
-            {session ? (
-               <Button
-                  onClick={() => {
-                     signOut({ redirect: false });
-                  }}
-               >
-                  Logout
-               </Button>
-            ) : (
-               <Button
-                  onClick={() => {
-                     router.push("/login");
-                  }}
-               >
-                  Login
-               </Button>
-            )}
-         </div>
+  return (
+    <div className="header">
+      <AutoComplete
+        dropdownMatchSelectWidth={252}
+        options={options}
+        className="search"
+        style={{ minWidth: "700px" }}
+        onSelect={onSelect}
+        value={valueSearch}
+        onSearch={handleSearch}
+      >
+        <Input placeholder="ស្វែងរក" suffix={<SearchOutlined style={{fontSize: '1.35rem'}} onClick={()=>{
+           console.log("click magnetic");
+        }} />} />
+      </AutoComplete>
+      <div className="headerInfo">
+        <div>
+          <p>
+            ក្រសួង-ស្ថាប័ន
+            <span style={{ color: "#6a0e00", fontWeight: "bold" }}>
+              យុត្តិធម័
+            </span>
+          </p>
+        </div>
+        <div>
+          <p>
+            ឈ្មោះអ្នកចូលប្រើ{" "}
+            <span style={{ color: "#6a0e00", fontWeight: "bold" }}>
+              {session && session.user?.firstName}
+            </span>
+          </p>
+        </div>
+        {session ? (
+          <Button
+            onClick={() => {
+              signOut({ redirect: false });
+            }}
+          >
+            Logout
+          </Button>
+        ) : (
+          <Button
+            onClick={() => {
+              router.push("/login");
+            }}
+          >
+            Login
+          </Button>
+        )}
       </div>
-   );
+    </div>
+  );
 };
 
 export default Header;
